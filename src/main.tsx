c3b38198607e77585e00bac5921ce6d53574bf34
hohre12 <60336857+hohre12@users.noreply.github.com>
2025-03-31 22:33:46 +0900

    UPDATE] prettier - eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router } from 'react-router-dom';
+import theme from './styles/variables.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        {/* auto complete is not avaliable for theme */}
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
+    </RecoilRoot>
+  </QueryClientProvider>,
+);
---
+++ src/styles/variables.ts
@@ -0,0 +1,9 @@
+const defaultColor = {
+  [`$blue-50`]: '#e5f0ff',
+};
+
+const palette = {
+  ...defaultColor,
+};
+
+export default palette;
